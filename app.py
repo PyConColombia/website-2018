efb6ea03e486c12f0aa2886843b8c520366088f7
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-07-09 11:14:04 -0500

    Add flask basic app and requirements

---
+++ app.py
@@ -0,0 +1,17 @@
+"""Main application."""
+
+# Third party imports
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
