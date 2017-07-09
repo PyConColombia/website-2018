b5fb05b74c48c5d1e3d79dc6dfdc00b593650cac
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-07-09 11:58:42 -0500

    Update requirements and app code style

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ from flask import render_template
 app = Flask(__name__)
 
 
+LANGUAGE_CODES = ('en', 'es')
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
